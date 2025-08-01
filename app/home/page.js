350db3c5c0eb09047dd98006e9329b976bd799d4
SongHyojun0228 <thdgywns2300@gmail.com>
2025-08-01 14:23:20 +0900

    url edit

--- app/home/page.js
+++ app/home/page.js
@@ -1,8 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Header from "../component/header";
-import Footer from "../component/footer";
 
 export default function HomePage() {
     const [url, setUrl] = useState("");
